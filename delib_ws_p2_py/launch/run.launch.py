abed03782b383052c0d7512268b9ee95b99ca2fe
Sebastian Castro <sebas.a.castro@gmail.com>
2024-08-26 21:16:11 -0400

    Update to pyrobosim 3.0.0

--- delib_ws_p2_py/launch/run.launch.py
+++ delib_ws_p2_py/launch/run.launch.py
@@ -1,9 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
--- delib_ws_p3/delib_ws_p3/run.py
+++ delib_ws_p3/delib_ws_p3/run.py
@@ -26,52 +26,27 @@ __Available Conditions__:
 import os
 import rclpy
 import threading
-import numpy as np
 
-from pyrobosim.core import Robot, World, WorldYamlLoader
+from pyrobosim.core import WorldYamlLoader
 from pyrobosim.gui import start_gui
-from pyrobosim.navigation import ConstantVelocityExecutor, PathPlanner
-from pyrobosim.utils.pose import Pose
 from pyrobosim_ros.ros_interface import WorldROSWrapper
 from ament_index_python.packages import get_package_share_directory
 
 
-data_folder = os.path.join(get_package_share_directory("delib_ws_p3"), "data")
-
-
-def create_world():
-    """Create a test world"""
-    # world = World()
-    world = WorldYamlLoader().from_yaml(os.path.join(data_folder, "world.yaml"))
-
-    # Set the location and object metadata
-    world.set_metadata(
-        locations=os.path.join(data_folder, "location_data.yaml"),
-        objects=os.path.join(data_folder, "object_data.yaml"),
+def create_world_from_yaml():
+    world_file = os.path.join(
+        get_package_share_directory("delib_ws_p3"), "data", "world.yaml"
     )
-
-    return world
-
-
-def create_world_from_yaml(world_file):
-    return WorldYamlLoader().from_yaml(os.path.join(data_folder, world_file))
+    return WorldYamlLoader().from_yaml(world_file)
 
 
 def create_ros_node():
     """Initializes ROS node"""
     rclpy.init()
     node = WorldROSWrapper(state_pub_rate=0.1, dynamics_rate=0.01)
-    node.declare_parameter("world_file", value="")
 
     # Set the world
-    world_file = node.get_parameter("world_file").get_parameter_value().string_value
-    if world_file == "":
-        node.get_logger().info("Creating demo world programmatically.")
-        world = create_world()
-    else:
-        node.get_logger().info(f"Using world file {world_file}.")
-        world = create_world_from_yaml(world_file)
-
+    world = create_world_from_yaml()
     node.set_world(world)
 
     return node
