d812eb93ad0c4231ec7331e3770afe14a1f4f358
Matthias Mayr <matthias.mayr@cs.lth.se>
2024-09-07 21:27:11 +0200

    New: Adds convenience functions and python warning suppression

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,10 +2,63 @@
 
 # Source ROS and the deliberation workspace.
 source /opt/ros/jazzy/setup.bash
+
+# Ignores additional output because of deprecated Python tooling in ament_python
+export PYTHONWARNINGS="ignore:setup.py install is deprecated,ignore:easy_install command is deprecated"
+
+export ROS_WS=/delib_ws
+export ROS_DOMAIN_ID=0
+
+function print_ros_variables () {
+        echo -e "ROS Distro: \t" $ROS_DISTRO
+        echo -e "ROS Domain ID: \t" $ROS_DOMAIN_ID
+        echo -e "ROS Workspace: \t" $ROS_WS
+}
+print_ros_variables
+
+# Convenience functions
+alias delib_src='cd $ROS_WS/src'
+alias delib_ws='cd $ROS_WS'
+# TODO(matthias-mayr): This ignored package is still being ported.
+function delib_build() {
+        cwd=$(pwd)
+        delib_ws
+        colcon build --symlink-install --continue-on-error --packages-ignore skiros2_task
+        cd $cwd
+}
+
+function delib_build_package() {
+        cwd=$(pwd)
+        delib_ws
+        colcon build --symlink-install --continue-on-error --packages-ignore skiros2_task --packages-select "$1"
+        cd $cwd
+}
+
+function delib_build_package_up_to() {
+        cwd=$(pwd)
+        delib_ws
+        colcon build --symlink-install --continue-on-error --packages-ignore skiros2_task --packages-up-to "$1"
+        cd $cwd
+}
+
+function delib_clean () {
+        cwd=$(pwd)
+        delib_ws
+        local _ret=$?
+        if [ $_ret -ne 0 ] ; then
+                echo "Could not switch dirs. Aborting delib_clean"
+                return $_ret
+        fi
+        rm -r build/*
+        rm -r install/*
+        rm -r log/*
+        cd $cwd
+}
+
+# Automatic build when entering the container
 if [ ! -f /delib_ws/install/setup.bash ]
 then
-  # TODO(matthias-mayr): This ignored package is still being ported.
-  colcon build --symlink-install --packages-ignore skiros2_task
+  delib_build
 fi
 source /delib_ws/install/setup.bash
 
